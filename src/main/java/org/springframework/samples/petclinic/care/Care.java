f28171e5032228f95cfb8d09488e426c11ba1be3
vicgrabru <vgravanbru@gmail.com>
2022-01-21 11:47:35 +0100

    Convocatoria lab

--- src/main/java/org/springframework/samples/petclinic/care/Care.java
+++ src/main/java/org/springframework/samples/petclinic/care/Care.java
@@ -2,6 +2,16 @@ package org.springframework.samples.petclinic.care;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
@@ -9,8 +19,23 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Care {
+@Entity
+@Table(name="cares")
+public class Care extends BaseEntity{
+	@NotEmpty
+	@Size(min=5, max=30)
+	@Column(unique=true)
     String name;
+	
+	@NotEmpty
     String description;
+	
+	@NotEmpty
+	@ManyToMany(cascade=CascadeType.ALL)
+	@JoinColumn(name="pet_type_id")
     Set<PetType> compatiblePetTypes;
+	
+	@ManyToMany(cascade=CascadeType.ALL, targetEntity=Care.class)
+	@JoinColumn(name="care_id")
+	Set<Care> incompatibleCares;
 }
--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -62,3 +62,16 @@ INSERT INTO visits(id,pet_id,visit_date,description) VALUES (2, 8, '2013-01-02',
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (3, 8, '2013-01-03', 'neutered');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (4, 7, '2013-01-04', 'spayed');
 
+INSERT INTO cares(id, name, description) VALUES (1, 'Hair brushing', 'We will brush the hair of your pet.');
+INSERT INTO cares(id, name, description) VALUES (2, 'Chemical flea removal', 'We will apply strong chemical products in the hair of your pet to remove any kind of flea or insect present.');
+
+INSERT INTO provisions(id, visit_id, duration, care_id) VALUES (1, 1, 0.5, 2);
+INSERT INTO provisions(id, visit_id, duration, care_id) VALUES (2, 2, 0.25, 1);
+
+INSERT INTO cares_compatible_pet_types VALUES (1, 1);
+INSERT INTO cares_compatible_pet_types VALUES (1, 2);
+INSERT INTO cares_compatible_pet_types VALUES (2, 1);
+INSERT INTO cares_compatible_pet_types VALUES (2, 2);
+
+INSERT INTO cares_incompatible_cares VALUES (1, 2);
+INSERT INTO cares_incompatible_cares VALUES (2, 1);
